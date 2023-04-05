97e12683a3e820b4e9691f26de491ac4dc5c9845
joly54 <perepelukdanilo@gmail.com>
2023-04-05 14:30:22 +0300

    return sql and add documentation

--- app.py
+++ app.py
@@ -8,16 +8,29 @@ import config
 from flask import Flask, jsonify, make_response, request
 from flask_restful import Api, Resource
 from email.message import EmailMessage
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
 
-users = {}
-try:
-    with open('users.json') as f:
-        users = json.load(f)
-except:
-    pass
+# Configuration for the database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize the database
+db = SQLAlchemy(app)
+
+
+# Define the User model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    token = db.Column(db.String(255), nullable=False)
+    secret_code = db.Column(db.String(8), nullable=False)
+
+    def __repr__(self):
+        return f"User(username='{self.username}', password='{self.password}', token='{self.token}', secret_code='{self.secret_code}')"
 
 
 def send_email(username, code):
@@ -52,9 +65,9 @@ def get_random_string(length):
 @app.after_request
 def after_request(response):
     try:
-        with open('users.json', 'w') as f:
-            json.dump(users, f)
+        db.session.commit()
     except Exception as e:
+        db.session.rollback()
         print(e)
     return response
 
@@ -63,21 +76,18 @@ def after_request(response):
 def handle_error(e):
     code = 500
     message = 'Internal server error' + str(e)
-    if hasattr(e, 'code'):
-        code = e.code
-        message = e.description
 
     return make_response(jsonify({'message': message}), code)
 
 
 class Login(Resource):
     def post(self):
-        #
         username = request.args.get('username')
         password = request.args.get('password')
-        if username not in users or users[username]['password'] != password:
+        user = User.query.filter_by(username=username).first()
+        if user is None or user.password != password:
             return {'error': 'Wrong username or password'}, 400
-        return {'message': 'Logged in successfully', 'token': users[username]['token']}, 200
+        return {'message': 'Logged in successfully', 'token': user.token}, 200
 
 
 class Register(Resource):
@@ -87,38 +97,37 @@ class Register(Resource):
         print(password)
         if username is None or password is None or not is_email(username):
             return {'error': 'Bad request'}, 400
-        if username not in users:
+        user = User.query.filter_by(username=username).first()
+        if user is None:
             token = get_random_string(32)
-            users[username] = {'password': password, 'token': token, "secretCode": get_random_string(8)}
+            secret_code = get_random_string(8)
+            user = User(username=username, password=password, token=token, secret_code=secret_code)
+            db.session.add(user)
+            db.session.commit()
             return {'message': 'User registered successfully', 'token': token}, 201
         return {'error': 'User already exists'}, 409
-
 
 class FogotPassword(Resource):
     def post(self):
         username = request.args.get('username')
-        if username not in users:
-            return {'message': 'User not found'}, 404
-        else:
-            users[username]['secretCode'] = get_random_string(8)
-            send_email(username, users[username]['secretCode'])
-            return {'message': 'Secret code sent to your email'}, 200
-
-
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            return {'error': 'User not found'}, 404
+        send_email(username, user.secret_code)
+        return {'message': 'Email sent successfully'}, 200
 class ResetPassword(Resource):
     def post(self):
         username = request.args.get('username')
-        secretCode = request.args.get('secretCode')
-        newPassword = request.args.get('newPassword')
-        if username not in users:
-            return {'message': 'User not found'}, 404
-        elif users[username]['secretCode'] != secretCode:
-            return {'message': 'Wrong secret code'}, 400
-        else:
-            users[username]['password'] = newPassword
-            users[username]['secretCode'] = get_random_string(8)
-            return {'message': 'Password changed successfully'}, 200
-
+        password = request.args.get('password')
+        secret_code = request.args.get('secret_code')
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            return {'error': 'User not found'}, 404
+        if user.secret_code != secret_code:
+            return {'error': 'Wrong secret code'}, 400
+        user.password = password
+        db.session.commit()
+        return {'message': 'Password reset successfully'}, 200
 
 api.add_resource(Login, '/login')
 api.add_resource(Register, '/register')
@@ -126,4 +135,6 @@ api.add_resource(FogotPassword, '/forgot-password')
 api.add_resource(ResetPassword, '/reset-password')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
