503d78efade92fa01597d53f769c9a225124a596
joly54 <perepelukdanilo@gmail.com>
2023-05-06 11:56:13 +0300

    some changes

--- hostapi.py
+++ hostapi.py
@@ -21,13 +21,16 @@ if response.status_code == 200:
 else:
     print("Error: Could not retrieve console data.")
 
+
 def GetOutput():
     response = requests.get(api_base_url + "/consoles/{id}/get_latest_output/".format(id=bconsole["id"]),
                             headers={"Authorization": "Token " + api_token})
     if response.status_code == 200:
-        print(response.json()["output"], end="")
+        print(f"{response.json()['output']}", end="")
     else:
         print("Error: Could not retrieve console output.")
+
+
 def SendInput(text):
     print("waiting for response...")
     response = requests.post(api_base_url + "/consoles/{id}/send_input/".format(id=bconsole["id"]),
@@ -35,9 +38,38 @@ def SendInput(text):
     print(response.text)
     if response.status_code != 200:
         print("Error: Could not send input to console.")
+
+
+is_reloading = False
+import time
+import threading
+def print_time():
+    curent_time = time.time()
+    while is_reloading:
+        print(f"reloading, seconds passed {round(time.time() - curent_time, 2)}", end="\r")
+        if not is_reloading:
+            break
+def reload():
+    global is_reloading
+    is_reloading = True
+    threading.Thread(target=print_time).start()
+    response = requests.post(api_base_url + "/webapps/vincinemaapi.pythonanywhere.com/reload/",
+                             headers={"Authorization": "Token " + api_token})
+    is_reloading = False
+    print(response.text)
+    if response.status_code != 200:
+        print("Error: webapp reload failed")
+    else:
+        print("------------------reloaded--------------------")
+
+
 while True:
     GetOutput()
-    SendInput(input())
-    os.system("cls")
-
-
+    command = input()
+    if command == "reload":
+        reload()
+    elif command == "exit":
+        exit()
+    else:
+        SendInput(command)
+        os.system("cls")
