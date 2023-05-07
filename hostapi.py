573d4e7773e77d8b439cd52a00c43d9a0eb45dc1
joly54 <perepelukdanilo@gmail.com>
2023-05-07 12:28:11 +0300

    updates in console

--- hostapi.py
+++ hostapi.py
@@ -1,30 +1,40 @@
 import os
 
 import requests
+import config
 
 api_base_url = "https://www.pythonanywhere.com/api/v0/user/vincinemaApi"
-api_token = '6085f0d381829a81ae376ffa70c250a422b2040a'
+api_token = config.api_token
 
-response = requests.get(api_base_url + "/consoles/", headers={"Authorization": "Token " + api_token})
-bconsole = None
-if response.status_code == 200:
-    consoles = response.json()
-    for console in consoles:
-        if console["executable"] == "bash":
-            bconsole = console
-            break
-    if bconsole is None:
-        print("Error: Could not find bash console.")
-        exit()
+consindex = -1
+consoles = []
+def GetConsoles():
+    global consindex, consoles
+    response = requests.get(api_base_url + "/consoles/", headers={"Authorization": "Token " + api_token})
+    if response.status_code == 200:
+        consoles = response.json()
+        print("Select console:")
+        for i in range(len(consoles)):
+            print(f"{i + 1}. {consoles[i]['name']}")
+        while True:
+            try:
+                consindex = int(input("Enter console index: "))
+                if consindex > len(consoles) or consindex < 1:
+                    print("Error: Invalid console index.")
+                else:
+                    consindex -= 1
+                    break
+            except ValueError:
+                print("Error: Invalid console index.")
     else:
-        print("Console found: " + bconsole["name"])
-else:
-    print("Error: Could not retrieve console data.")
+        print("Error: Could not retrieve console data.")
+        exit()
+
+GetConsoles()
 
 
 def GetOutput():
-    response = requests.get(api_base_url + "/consoles/{id}/get_latest_output/".format(id=bconsole["id"]),
-                            headers={"Authorization": "Token " + api_token})
+    response = requests.get(api_base_url + "/consoles/{id}/get_latest_output/".format(id=consoles[consindex]["id"]),headers={"Authorization": "Token " + api_token})
     if response.status_code == 200:
         os.system("cls")
         print(f"{response.json()['output']}", end="")
@@ -34,7 +44,7 @@ def GetOutput():
 
 def SendInput(text):
     print("waiting for response...")
-    response = requests.post(api_base_url + "/consoles/{id}/send_input/".format(id=bconsole["id"]),
+    response = requests.post(api_base_url + "/consoles/{id}/send_input/".format(id=consoles[consindex]["id"]),
                              data={"input": text + "\n"}, headers={"Authorization": "Token " + api_token})
     print(response.text)
     if response.status_code != 200:
@@ -109,5 +119,17 @@ while True:
         reload(0)
     elif command == "url":
         os.system("start https://vincinemaapi.pythonanywhere.com/")
+    elif command == "chconsole":
+        GetConsoles()
+    elif command == "help":
+        print('''
+        reload - reloads webapp
+        reload -r - reloads webapp without uploading
+        url - opens webapp url
+        upload - uploads app.py
+        chconsole - changes console
+        exit - exits
+        ''')
+        input("press enter to continue")
     else:
         SendInput(command)
