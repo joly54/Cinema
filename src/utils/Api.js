eab050b5ad7c0f5d42414465e157c97c322e91f4
joly54 <danyla958@gmail.com>
2023-06-03 23:59:11 +0300

    returned original api url

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,4 +1,4 @@
-export const baseurl = "http://127.0.0.1:5000";
+export const baseurl = "https://vincinemaapi.pythonanywhere.com/";
 
 export const login = (username, password) => {
     const json = JSON.stringify({username: username, password: password})
